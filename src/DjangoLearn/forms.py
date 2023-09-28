d2d15d2405b0e20e6a0f78e49f7d3ba15ab4c004
fsabuhi <93775724+fsabuhi@users.noreply.github.com>
2023-09-29 00:30:45 +0400

    Added forms and models for Course data

--- src/DjangoLearn/forms.py
+++ src/DjangoLearn/forms.py
@@ -4,8 +4,13 @@ from django.contrib.auth.forms import AuthenticationForm
 class UserForm(forms.Form):
     username = forms.CharField(label="username", max_length=100)
     password = forms.CharField(label="password", max_length=100)
+    type = forms.ChoiceField(choices=[('teacher', 'Teacher'), ('student', 'Student')])
 
 class CustomAuthenticationForm(AuthenticationForm):
     error_messages = {
         'invalid_login': 'Invalid username or password',
     }
+
+class CourseForm(forms.Form):
+    name = forms.CharField(label="name", max_length=100)
+    description = forms.CharField(label="description", max_length=100)
--- src/DjangoLearn/models.py
+++ src/DjangoLearn/models.py
@@ -1,15 +1,15 @@
-# import the standard Django Model
-# from built-in library
 from django.db import models
-  
-# declare a new model with a name "GeeksModel"
+from django.contrib.auth.models import User
+
 class UserModel(models.Model):
- 
-    # fields of the model
-    title = models.CharField(max_length = 200)
-    description = models.TextField()
- 
-    # renames the instances of the model
-    # with their title name
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    type = models.CharField(max_length=10, choices=[('teacher', 'Teacher'), ('student', 'Student')])
     def __str__(self):
-        return self.title
+        return self.user.username
+
+class CourseModel(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name        
